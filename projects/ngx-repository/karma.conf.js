a71b145f6be53f1a1c2b094e698c17011be2c66b
thomasnisole <thomasnisole@gmail.com>
2020-04-15 11:07:31 +0200

    Fix Karma conf

--- projects/ngx-repository/karma.conf.js
+++ projects/ngx-repository/karma.conf.js
@@ -31,7 +31,7 @@ module.exports = function (config) {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
       }
-    }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
