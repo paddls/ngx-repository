b32b704ff979b92f095ffb254de107c5f7fb4611
Thomas Nisole <thomas.nisole@witty-services.fr>
2020-06-29 14:46:21 +0200

    feat: Complete message for firebase message error

--- projects/ngx-firebase-repository/src/lib/error/ngx-firebase-repository.error.ts
+++ projects/ngx-firebase-repository/src/lib/error/ngx-firebase-repository.error.ts
@@ -1,10 +1,22 @@
 import {FirebaseRequest} from '../firebase.request';
 import {FirebaseError} from 'firebase';
+import {omitBy} from 'lodash';
+
+const IGNORED_KEYS: string[] = ['paths', 'readPath', 'createPath', 'updatePath', 'deletePath', 'pathParams', 'replaceParams'];
 
 export class NgxFirebaseRepositoryError extends Error {
 
   public constructor(path: string, request: FirebaseRequest<any>, firebaseError: FirebaseError) {
-    super(`An error occurred when the path '${path}' was requested : ${firebaseError.message}`);
+    let message: string = `An error occurred when the path '${path}' was requested : ${firebaseError.message}`;
+    const r: any = omitBy(request, (v: any, key: string) => {
+      return IGNORED_KEYS.indexOf(key) !== -1 || v == null || (Array.isArray(v) && v.length === 0);
+    });
+
+    if (Object.keys(r).length > 0) {
+      message += ` The associated request is ${JSON.stringify(r)}.`;
+    }
+
+    super(message);
 
     this.name = 'NgxFirebaseRepositoryError';
   }
