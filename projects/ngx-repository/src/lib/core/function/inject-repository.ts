29e979d3e791e47ead02862fff905a73936db217
Thomas Nisole <thomasnisole@gmail.com>
2025-04-16 14:41:36 +0200

    feat: format code

--- projects/ngx-repository/src/lib/core/function/inject-repository.ts
+++ projects/ngx-repository/src/lib/core/function/inject-repository.ts
@@ -1,6 +1,6 @@
-import {NgxRepositoryService} from '../../ngx-repository.service';
-import {inject} from '@angular/core';
-import {InjectRepositoryContext} from '../configuration/context/inject-repository-context.configuration';
+import { NgxRepositoryService } from '../../ngx-repository.service';
+import { inject } from '@angular/core';
+import { InjectRepositoryContext } from '../configuration/context/inject-repository-context.configuration';
 
 export function injectRepository<T>(params: InjectRepositoryContext<T>): any {
   return inject(NgxRepositoryService).getRepository(params.resourceType(), params.repository());
